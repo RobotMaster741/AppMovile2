32829fb9fbe7e40b644d7a0761aa9f75bcbc2f99
RobotMaster741 <moisesgutierrezmlg@gmail.com>
2023-11-07 16:34:48 -0300

    subido

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,5 @@
 export const environment = {
   production: true,
-  /*configuracion del nico firebase: {
-    apiKey: "AIzaSyB_BJVh4TUHow4piyCMAk5p9wVYpd8gSOc",
-    authDomain: "duoc-transport.firebaseapp.com",
-    projectId: "duoc-transport",
-    storageBucket: "duoc-transport.appspot.com",
-    messagingSenderId: "1018212150860",
-    appId: "1:1018212150860:web:51bbd5c30932572d968ee9",
-    measurementId: "G-16PWTTT41K"
-  },*/
 
   firebase: {
     apiKey: "AIzaSyCpP2tky5q6623EyFXnr5yORQeLAIqJR2M",
